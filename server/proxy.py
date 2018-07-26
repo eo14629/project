53a1334e37468a9537309358fccc797b3b2c9bb6
Eddie O'Donnell <eo14629@my.bristol.ac.uk>
2018-07-26 20:57:47 +0100

    created first draft of the proxy server which will allow multiple requests to be bundled together and sent from the client as just one reqeuest

---
+++ server/proxy.py
@@ -0,0 +1,58 @@
+import BaseHTTPServer
+import requests
+import socket
+import time
+
+HOST_NAME = socket.gethostbyname(socket.gethostname())
+PORT_NUMBER = 8082
+SERVER_NAME = "http://192.168.0.34:8081/"
+
+class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """Handle the Responses coming in from the Client"""
+    def do_GET(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("path accessed: %s" % s.path)
+
+        # split the path into different sections
+        extns = s.path.split('/')
+        if len(extns) > 2:
+            responses = bundle(extns)
+            print responses
+            s.wfile.write(responses)
+        else:
+            response = getRequest(extns[1])
+            print response
+            s.wfile.write(response)
+
+# a bundling method to send and receive multiple requests
+# to the main server and then package them up in a list.
+def bundle(extns):
+    responses = ['']
+    responses.pop(0)
+    extns.pop(0)
+    for extension in extns:
+        responses.append(getRequest(extension))
+    return responses
+
+# standard get request to the main server
+# this will be used in the bundling method repeatedly
+def getRequest(extension):
+    url = SERVER_NAME + extension
+    server_response = requests.get(url)
+    return server_response.text
+
+# set up the proxy server
+def run():
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), Handler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+
+run()
